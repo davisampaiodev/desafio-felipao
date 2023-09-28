78e4f876181e03e79d2ed70be4d093ae3a437515
davisampaiodev <davidsaps10@outlook.com>
2023-09-28 15:32:37 -0300

    desafiofinalizado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log("Olá mundo") 
+let nomedoheroi = "Sampa"
+let xpdoheroi = 2000
+
+if (xpdoheroi <=1000){
+console.log("O heroi de nome " + nomedoheroi + " está no nivel de Ferro")
+}
+
+if (xpdoheroi >= 1001 && (xpdoheroi <= 2000)){
+    console.log("O heroi de nome " + nomedoheroi + " está no nivel de Bronze")
+    }
+
+    if (xpdoheroi >= 2001 && (xpdoheroi <= 5000)){
+        console.log("O heroi de nome " + nomedoheroi + " está no nivel de Prata")
+        }
+
+        if (xpdoheroi >= 6001 && (xpdoheroi <= 7000)){
+            console.log("O heroi de nome " + nomedoheroi + " está no nivel de Ouro")
+            }
+
+            if (xpdoheroi >= 7001 && (xpdoheroi <= 8000)){
+                console.log("O heroi de nome " + nomedoheroi + " está no nivel de Platina")
+                }
+                
+                if (xpdoheroi >= 8001 && (xpdoheroi <= 9000)){
+                    console.log("O heroi de nome " + nomedoheroi + " está no nivel de Ascendente")
+                    }
+
+                    if (xpdoheroi >= 9001 && (xpdoheroi <= 10000)){
+                        console.log("O heroi de nome " + nomedoheroi + " está no nivel de Imortal")
+                        }
+
+                        if (xpdoheroi >= 10001){
+                            console.log("O heroi de nome " + nomedoheroi + " está no nivel de Radiante")
+                            }
